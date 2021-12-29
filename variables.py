dd81d3a8ce20eaea32c0f4f464fe0643a9d2de39
alexmmeyer <67715044+alexmmeyer@users.noreply.github.com>
2021-12-29 13:21:37 -0500

    Update variables.py
    Lambda explanation

--- variables.py
+++ variables.py
@@ -10,4 +10,5 @@ RANKING_AS_OF = "08/12/2018"
 # Depreciation Period: time period in days over which a depreciation is applied to the initial weight of a result.
 DEPRECIATION_PERIOD = 365 * 1.2
 
+# Drives age_weight_exp() exponential decay function. The more negative, the quicker the decline in age_weight.
 LAMBDA = -2
