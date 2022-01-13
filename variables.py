4b3945c8f6bbe1bcd8626d72174e059836098691
alexmmeyer <67715044+alexmmeyer@users.noreply.github.com>
2022-01-13 12:50:08 -0500

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,16 +1,17 @@
 import pandas
 
 event_points = pandas.read_csv("event_points.csv")
-DIRECTORY = "results"
+RESULTS_DIRECTORY = "results"
 RANKING_FILE_NAME = "PageRanking.csv"
 
 # Must be in MM/DD/YYYY format:
-RANKING_AS_OF = "08/12/2018"
+RANKING_AS_OF = "08/20/2018"
 
 # Depreciation Period: time period in days over which a depreciation is applied to the initial weight of a result.
-DEPRECIATION_PERIOD = 365 * 1.5
-
-aging_method = "exponential"
+DEPRECIATION_PERIOD = 365 * 2
 
 # Drives age_weight_exp() exponential decay function. The more negative, the quicker the decline in age_weight.
 LAMBDA = -2
+
+# Tune ranking for the following distance in km. 0 for no preference, or best "overall" regardless of dist.
+RANK_DIST = 10
